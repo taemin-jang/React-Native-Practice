b0c88a6f9502edab0f0eac2c4ea40a6b4375a37d
taemin-Jang <jtm990506@gmail.com>
2023-11-22 17:32:41 +0900

    Weather App 완성

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,16 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
+import { Fontisto } from '@expo/vector-icons';
 const {width:SCREEN_WIDTH} = Dimensions.get('window');
+const API_KEY = '0abff7496354b2d1a47f62e16a7cf464';
 
-const API_KEY = 'c1cd2de7d8a43e73852b8f643f8bc83f';
+const icons = {
+  "Clouds" : "cloudy",
+  "Clear": "day-sunny",
+  "Snow": "snow",
+  "Rain": "rains",
+  "Drizzle": "rain",
+  "Thunderstorm": "lightning",
+  "Atmosphere": "cloudy-gusts"
+}
 
 export default function App() {
   const [street, setStreet] = useState(null);
   const [days, setDays] = useState(null);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
    const {granted} = await Location.requestForegroundPermissionsAsync()
    if(!granted){
     setOk(false)
@@ -18,12 +28,13 @@ export default function App() {
    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5})
    const location = await Location.reverseGeocodeAsync({latitude, longitude})
    setStreet(location[0].street)
-   const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-   const json = await response.json();
-   console.log(json)
+   const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+   const {list} = await response.json()
+   const filterList = list.filter(({dt_txt}) => dt_txt.endsWith("03:00:00"))
+   setDays(() => filterList)
   }
   useEffect(() => {
-    ask();
+    getWeather();
   }, [])
   return (
       <View style={styles.container}>
@@ -31,22 +42,22 @@ export default function App() {
           <Text style={styles.cityName}>{street}</Text>
         </View>
         <ScrollView pagingEnabled showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={styles.weather}>
-          <View style={styles.day}>
-            <Text style={styles.temp}>27</Text>
-            <Text style={styles.description}>Sunny</Text>
-          </View>
-          <View style={styles.day}>
-            <Text style={styles.temp}>27</Text>
-            <Text style={styles.description}>Sunny</Text>
-          </View>
-          <View style={styles.day}>
-            <Text style={styles.temp}>27</Text>
-            <Text style={styles.description}>Sunny</Text>
-          </View>
-          <View style={styles.day}>
-            <Text style={styles.temp}>27</Text>
-            <Text style={styles.description}>Sunny</Text>
-          </View>
+          {days === null ? 
+            <View style={{...styles.day, alignItems: "center"}}>
+              <ActivityIndicator color='white' size={'large'} style={{marginTop: 10}} />
+            </View> : 
+            days.map((day, index) => 
+              <View key={index} style={styles.day}>
+                <Text style={styles.date}>{day.dt_txt.slice(5,10)}</Text>
+                <View style={{flexDirection: 'row', width:SCREEN_WIDTH, alignItems:'flex-end', justifyContent:'space-between'}}>
+                  <Text style={styles.temp}>{+day.main.temp.toFixed(1)}</Text>
+                  <Fontisto name={icons[day.weather[0].main]} size={100} color="white"  />
+                </View>
+                <Text style={styles.description}>{day.weather[0].main}</Text>
+                <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+              </View>
+            ) 
+          }
         </ScrollView>
       </View>
   );
@@ -60,25 +71,38 @@ const styles= StyleSheet.create({
   city: {
     flex:1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   cityName: {
-    fontSize: 68
+    fontSize: 68,
+    color:"white"
   },
   weather: {
-    backgroundColor: 'red'
+    // backgroundColor: 'red'
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start"
+  },
+  date: {
+    fontSize: 40,
+    marginTop:50,
+    marginBottom:-80,
+    color:"white"
   },
   temp: {
     marginTop: 50,
-    fontSize: 178
+    fontSize: 100,
+    color:"white"
+  },
+  tinyText: {
+    fontSize:30,
+    color:"white"
   },
   description: {
     marginTop: -30,
-    fontSize: 60
+    fontSize: 40,
+    color:"white"
   }
 })
 
